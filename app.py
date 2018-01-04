8d2b872093c2ca6ec46ec673ecd474b05af45472
iamchu <v235711@gmail.com>
2018-01-04 19:05:24 -0200

    Nearly all done, all logic for handling dbs has been done. Now its just fine tuning and speeding shit up

--- app.py
+++ app.py
@@ -28,24 +28,31 @@ def returnCompanyCodesAsList():
 
 
 # creates a respective table for the respective company in the db passed as argument
-def tableConstructor(db, table_name):
-	con = lite.connect('test.db')
+def createTable(db, table_name):
+	con = lite.connect(db)
+	table_name = table_name.replace(".", "_")
+	with con:
+		cur = con.cursor()
+		sqlite_command_create = "CREATE TABLE "+ table_name +"(data TEXT, cotacao REAL, minima REAL, maxima REAL, variacao REAL, variacao_porcentagem REAL, volume INT)"
+		sqlite_command_drop = "DROP TABLE IF EXISTS "+ table_name
+		cur.execute(sqlite_command_drop)
+		cur.execute(sqlite_command_create)
+	if con:
+		con.close()
 
-	# with con:
-	    
-	#     cur = con.cursor()    
-	#     cur.execute("CREATE TABLE Cars(Id INT, Name TEXT, Price INT)")
-	#     cur.execute("INSERT INTO Cars VALUES(1,'Audi',52642)")
-	#     cur.execute("INSERT INTO Cars VALUES(2,'Mercedes',57127)")
-	#     cur.execute("INSERT INTO Cars VALUES(3,'Skoda',9000)")
-	#     cur.execute("INSERT INTO Cars VALUES(4,'Volvo',29000)")
-	#     cur.execute("INSERT INTO Cars VALUES(5,'Bentley',350000)")
-	#     cur.execute("INSERT INTO Cars VALUES(6,'Citroen',21000)")
-	#     cur.execute("INSERT INTO Cars VALUES(7,'Hummer',41400)")
-	#     cur.execute("INSERT INTO Cars VALUES(8,'Volkswagen',21600)")
+# argument is one list with the 7 values we need for the company data
+def insertIntoTable(db,list_with_data, table_name):
+	con = lite.connect(db)
+	table_name = table_name.replace(".", "_")
+	with con: 
+		cur = con.cursor()
+		sqlite_command_insert = "INSERT INTO " + table_name + " VALUES(" + list_with_data[0]+","+list_with_data[1] + "," + list_with_data[2] + "," + list_with_data[3] + "," + list_with_data[4] + "," + list_with_data[5] + "," + list_with_data[6] +")"
+		cur.execute(sqlite_command_insert)
+	if con:
+		con.close()
 
-def insertStockDataIntoTable(data, cotacao, minima, maxima, variacao, variacao_porcentagem, volume, table):
-	pass
+# def insertStockDataIntoTable(data, cotacao, minima, maxima, variacao, variacao_porcentagem, volume, table):
+	# pass
 
 # populate db with data from url passed as argument
 def scrapeCompanyQuotationsAndReturnAsListOfLists(company_code):
@@ -57,20 +64,60 @@ def scrapeCompanyQuotationsAndReturnAsListOfLists(company_code):
 	# go through each of the items and break in a sublist and grab the corresponding items accordingly
 
 	soup = bs4.BeautifulSoup(page.text, "lxml")
+	tblInterday = soup.find(id="tblInterday")	
+	soup = bs4.BeautifulSoup(str(tblInterday), "lxml")	
 
 	lines = soup.find_all("tr")
+	company_historical_data = []
 
-	for i in range(len(lines)):
+	# Here the range starts at 1 because the tblInterday has headers
+	for i in range(1,len(lines)):
+		# holds the data for one line of the historical quotations table. This is so we can append to the company_historical_data
+		current_line_historical_data = []
 		line_soup = bs4.BeautifulSoup(str(lines[i]), "lxml")
 
 		line_values = line_soup.find_all("td")
-		print len(line_values)
-		for j in range(len(line_values)):
-			print line_values[0].get_text() + " " + line_values[1].get_text()
 
+		for j in range(len(line_values)):
+			if j < 6:
+				current_line_historical_data.append(line_values[j].get_text().replace(",", "."))
+			else:
+				current_line_historical_data.append(line_values[j].get_text().replace(".", ""))
+		company_historical_data.append(current_line_historical_data)
+
+	return company_historical_data
+
+
+def handleDbInserting():
+	pass
 
 def main():
-	list_of_company_codes = returnCompanyCodesAsList()
-	scrapeCompanyQuotationsAndReturnAsListOfLists("ITUB4.SA")
+	# list_of_company_codes = returnCompanyCodesAsList()
+		
+	data = scrapeCompanyQuotationsAndReturnAsListOfLists("VIVR3.SA")
+	for i in range(0,10):
+		print data[i]
+	# for line in data:
+	# 	if len(line) > 7:
+	# 		print line
+
+	test_list = ["VIVR3.SA","ITUB4.SA"]
+
+	for company in test_list:
+		total_entries = 0
+		createTable('stock_data.db', company)
+		data = scrapeCompanyQuotationsAndReturnAsListOfLists(company)
+		for line in data:
+			print "Inserting " + str(line) + " into table " + company
+			insertIntoTable('stock_data.db',line, company)
+			total_entries+=1
+		print "Total entries: " + total_entries
+		print "Sucessfully created and inserted data into table " + company
+
 
 main()
+
+# todo now:
+# 1) save the data from the tables to a table in the correct database.
+
+# TA DANDO ERRO DO 12 VALUES WERE SUPPLIED POR CAUSA DAS BARRAS E DOS PONTOS NOS NUMEROS INSERIDOS! A TABELA PRECISA DE , PRA FLOAT E ACHO Q BARRAS TALVES SEJAM PROBLEMATICAS (?)
